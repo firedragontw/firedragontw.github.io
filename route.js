9d241c807d70b5d21ccf4fe89644557cc38b8bd5
firedragon <jeremyyihsinchang@gmail.com>
2026-01-11 23:30:47 +0800

    a

--- route.js
+++ route.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'eng': '/eng.html',
         'bus': '/bus.html', 
         'enyu': '/enyu.html',
-        'gsat': '/gsatouo.html',
+        'gsatouo': '/gsatouo.html',
     };
     
     // 確定要載入的 HTML 檔案
